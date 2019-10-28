911212c77e7af440ee10174b0ac77084764dfa7d
Gabriel Nascimento <gabriel.amorim.nascimento@gmail.com>
2019-10-28 17:36:23 -0300

    Add if/else section

--- micro.py
+++ micro.py
@@ -19,11 +19,78 @@ if __name__ == "__main__":
         write("r{} DD ?".format(i))
     write("")
 
+    label = 0
+    tem_else = False
+    x, y, z = None, None, None
     for line in text:
         args = line.strip().split()
 
+        # ----- Desvio ------
+        if args[0] == "if":
+            cmp = {
+                "==":"je",
+                "!=":"jne",
+                ">":"jg",
+                ">=":"jge",
+                "<":"jl",
+                "<=":"jle"
+            }
+
+            anti_op = {
+                "je":"jne",
+                "jne":"je",
+                "jg":"jle",
+                "jle":"jg",
+                "jl":"jge",
+                "jge":"jl"
+            }
+
+            op = cmp[args[2]]
+            x, y, z = args[1], args[3], anti_op[op]
+
+            if x.isdecimal():
+                write("mov eax {}".format(x))
+            else:
+                write("mov eax [{}]".format(x))
+            
+            if y.isdecimal():
+                write("mov ebx {}".format(y))
+            else:
+                write("mov ebx [{}]".format(y))
+
+            write("cmp eax ebx")
+            write("{} #if{}".format(op, label))
+            write("jmp #endif{}".format(label))
+            write("#if{}".format(label))
+
+        # ----- Desvio Cont. -----
+        elif args[0] == "else":
+            write("#else{}".format(label))
+            tem_else = True
+        
+        # ----- Fim Desvio -----
+        elif args[0] == "endif":
+            write("#endif{}".format(label))
+            if tem_else:
+                tem_else = False
+
+                if x.isdecimal():
+                    write("mov eax {}".format(x))
+                else:
+                    write("mov eax [{}]".format(x))
+                
+                if y.isdecimal():
+                    write("mov ebx {}".format(y))
+                else:
+                    write("mov ebx [{}]".format(y))
+
+                write("cmp eax ebx")
+                write("{} #else{}".format(z, label))
+            
+            label += 1
+
         # ----- Atribuição -----
-        if args[1] == "=":
+        elif args[1] == "=":
 
             # ri = expr op expr
             if len(args) == 5:
